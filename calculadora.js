13c2bf9db0606bde3cf0a96eed1bd26f4a934a86
marcio56 <samurai.kio33@gmail.comm>
2023-09-23 11:56:32 -0300

    first commit

---
+++ calculadora.js
@@ -0,0 +1,10 @@
+var mes = document.querySelector("#mes")
+var horas = document.querySelector("#horas")
+var dias = document.querySelector("#dias")
+var resposta = document.querySelector("#resposta")
+
+
+function calculadorahora(){
+    var valorHora = (mes.valueAsNumber /dias.valueAsNumber)/horas.valueAsNumber
+    resposta.textContent = valorHora
+}
